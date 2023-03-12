4d5805857380e52d9d6084f9f541acaea3783b96
MsterAB <ducahmed@icloud.com>
2023-03-12 16:04:30 -0400

    trying to implement file viewing (still WIP)

--- wdl/settings.py
+++ wdl/settings.py
@@ -126,4 +126,5 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+import os
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
